c8d94ec4815c5d605df0bb5dfb982c30d4ef92aa
Victor Mak <contact@htetaung.com>
2024-11-16 05:00:54 +0700

    feat: struct setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,45 @@
 //!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
+//! Stylus Prediction Contract
+//! 
+//! This contract allows users to place predictions on running matches
 
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
+use alloy_sol_types::sol;
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::{StorageAddress, StorageBool, StorageU256, StorageVec}};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
-    }
+sol!{
+    event prediction_results_available(uint256 indexed match_id, bool result);
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+#[storage]
+struct PredictionPool{
+    exists: StorageBool,
+
+    player1_predictor_count: StorageU256,
+    player1_predictor : StorageVec<StorageAddress>,
+    player1_predictor_stake : StorageVec<StorageU256>,
+    player1_pool_stake: StorageU256,
+
+    player2_predictor_count: StorageU256,
+    player2_predictor : StorageVec<StorageAddress>,
+    player2_predictor_stake : StorageVec<StorageU256>,
+    player2_pool_stake: StorageU256,
+
+    total_staked: StorageU256
+}
+
+#[storage]
+#[entrypoint]
+pub struct PredictionContract{
+    initialized: StorageBool,
+    owner: StorageAddress,
+}
+
 #[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
-    }
+impl PredictionContract{
+    
 }
